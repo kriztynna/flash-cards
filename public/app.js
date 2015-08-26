ada437f254044a0d22a1d20e9d1ac6397e15956e
Cristina <cristina.colon+git@gmail.com>
2015-08-26 17:21:14 -0400

    flash cards with categories

--- public/app.js
+++ public/app.js
@@ -1,5 +1,19 @@
 var app = angular.module('flashCards', []);
 
+app.factory('FlashCardsFactory', function ($http) {
+    // return { justTesting: 'testing!' };
+    return {
+        getFlashCards: function (category) {
+            if (category){
+                return $http.get('/cards?category='+category)
+                    .then(function(response){return response.data});
+            }
+            return $http.get('/cards')
+                .then(function(response){return response.data});
+        }
+    };
+});
+
 app.value('whateverName', [
     {
         question: 'C. What is Angular?',
--- public/main.controller.js
+++ public/main.controller.js
@@ -1,10 +1,28 @@
-app.controller('MainController', function ($scope, whateverName) {
-	$scope.flashCards = whateverName;
+app.controller('MainController', function ($scope, whateverName, FlashCardsFactory) {
+	$scope.categories = ['MongoDB','Express','Angular','Node'];
+	$scope.currentCategory;
+
+	FlashCardsFactory.getFlashCards().then(function (flashCards) {
+		$scope.flashCards = flashCards;
+	}).then(null, function (error) {
+		console.log("Error", error);
+	});
+
+	$scope.getCategoryCards = function (category) {
+		$scope.currentCategory = category;
+		FlashCardsFactory.getFlashCards(category)
+			.then(function (cards) {
+				$scope.flashCards = cards;
+			})
+			.then(null, function (error) {
+				console.error("Error getting category cards.");
+			});
+	};
 
 	$scope.answerQuestion = function (answer, flashCard) {
 		if (!flashCard.answered) {
 			flashCard.answered = true;
 			flashCard.answeredCorrectly = answer.correct;
 		}
-	}
+	};
 });
