92fefefcf26ca8d777ce98f23acab82a69f0d1cf
Chandra Galbiati <chandragalbiati@Christophers-MacBook-Air.local>
2015-08-27 16:16:13 -0400

    about to scope isolate

---
+++ public/js/directives/flashCard/flashCardDirective.js
@@ -0,0 +1,8 @@
+app.directive('flashCardDirective', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'js/directives/flashCard/flashCardDirective.html',
+		// link: function(scope, element, arttribute){},
+		// scope: { card: '='}
+	};
+});
